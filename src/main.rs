96314d87cffe5fe58a1509a346cd340b8d54797b
hababisha <haileamlakderbie1@gmail.com>
2024-11-22 23:50:37 +0300

    quotes vec

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use rand::seq::SliceRandom;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
 fn main() {
-    println!("Tks on the terminal!");
-    println!("Random Quotes whenever you open the terminal and you can add to the quotes");
+    
+    let quotes = vec![
+        "Life is what happens when you're busy making other plans.",
+        "The best way to predict the future is to invent it.",
+        "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment."
+    ];
+    let quote = quotes.choose(&mut rand::thread_rng()).unwrap();
+    println!("{}", quote);
+
 }
